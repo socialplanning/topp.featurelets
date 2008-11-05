1289e91b3fc6b86271fa8253dd6cd5858f3755cf
Paul Winkler <slinkp@gmail.com>
2008-11-05 20:18:50 +0000

    Bookkeeping for topp.featurelets release: ADDED a bit of info to README.txt REMOVED a bit of wordiness from README.txt ADDED long_description from readme.txt CHANGED topp.utils dependency to >= 0.5 ADDED trove classifiers to setup.py
    git-svn-id: https://svn.socialplanning.org/svn/topp.featurelets/tags/0.3.0@21247 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ version = '0.3.0'
 
 setup(name='topp.featurelets',
       version=version,
-      description="Base support for featurelets (i.e. feature plugins)",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      keywords='featurelets openplans topp',
+      description="Base support for plone featurelets (i.e. feature plugins)",
+      long_description=open('topp/featurelets/README.txt').read(),
+      classifiers=['Framework :: Plone', 'Framework :: Zope2',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Programming Language :: Python :: 2',
+                   ],
+      # classifier strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='featurelets openplans plone topp',
       author='Rob Miller',
       author_email='robm@openplans.org',
       url='https://svn.openplans.org/svn/topp.featurelets/',
@@ -19,7 +22,7 @@ setup(name='topp.featurelets',
       zip_safe=False,
       install_requires=[
           # -*- Extra requirements: -*-
-          'topp.utils',
+          'topp.utils>=0.5',
       ],
       dependency_links=[],
       entry_points="""
